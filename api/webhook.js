eb58ca1366e44b1d2dc41413579293b82bd82a67
soIoWAY <96046858+soIoWAY@users.noreply.github.com>
2024-07-16 11:16:32 +0300

    weather refactoring

--- api/webhook.js
+++ api/webhook.js
@@ -1,6 +1,6 @@
 require('dotenv').config()
 const { Telegraf } = require('telegraf')
-const axios = require('axios')
+const weatherCommandHandler = require('../handlers/weatherCommand')
 
 const token = process.env.TOKEN
 const bot = new Telegraf(token)
@@ -30,26 +30,7 @@ bot.start(async ctx => {
 	await sendMessages(ctx, messages, 800)
 })
 
-bot.command('weatherNow', async ctx => {
-	try {
-		const weatherApi =
-			'https://api.open-meteo.com/v1/forecast?latitude=49.78&longitude=23.64&current=temperature_2m,wind_speed_10m&hourly=temperature_2m,relative_humidity_2m,wind_speed_10m&timezone=Europe/Kiev'
-
-		const res = await axios.get(weatherApi)
-		const temp = res.data.current.temperature_2m
-		const wind = res.data.current.wind_speed_10m
-		const isoTime = res.data.current.time
-		const date = new Date(isoTime)
-		let hours = date.getHours()
-		let minutes = date.getMinutes()
-		let formattedTime = `${hours}:${minutes < 10 ? '0' : ''}${minutes}`
-		await ctx.reply(
-			`Час: ${formattedTime} ⏰\nТемпература: ${temp}°C 🌡\nШвидкість вітру: ${wind}km/h 💨`
-		)
-	} catch (err) {
-		console.error(err)
-	}
-})
+bot.command('weatherNow', weatherCommandHandler)
 
 module.exports = async (req, res) => {
 	await bot.handleUpdate(req.body, res)
