2ebe617633159be2c4113ec679ff2f1458b11281
soIoWAY <vitalikmur38@gmail.com>
2024-07-17 22:50:17 +0300

    gojo checker

--- handlers/gojo/purpleCommandHandler.js
+++ handlers/gojo/purpleCommandHandler.js
@@ -1,11 +1,16 @@
 const sendMessages = require('../sendMessages')
 async function purpleCommandHandler(ctx) {
-	const messages = [
-		'Phase twilight\nEyes of wisdom\nNine ropes',
-		'Crow and declaration\nBetween front and back\nHollow... Purple',
-		'https://media1.tenor.com/m/DSyo0NKX8gMAAAAC/gojo-satoru.gif',
-	]
-	await sendMessages(ctx, messages, 800)
+	const username = ctx.message.from.username
+	if (username !== 'xzvetal') {
+		await ctx.reply('Ти не Годжо Сатору!')
+	} else {
+		const messages = [
+			'Phase twilight\nEyes of wisdom\nNine ropes',
+			'Crow and declaration\nBetween front and back\nHollow... Purple',
+			'https://media1.tenor.com/m/DSyo0NKX8gMAAAAC/gojo-satoru.gif',
+		]
+		await sendMessages(ctx, messages, 800)
+	}
 }
 
 module.exports = purpleCommandHandler
