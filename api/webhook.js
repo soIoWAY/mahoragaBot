5c4a58337bf5e5e540603b4cecb386ca17dcfc89
soIoWAY <vitalikmur38@gmail.com>
2024-07-18 16:26:15 +0300

    upd tb

--- api/webhook.js
+++ api/webhook.js
@@ -255,7 +255,7 @@ bot.on('text', async ctx => {
 					await new Promise(resolve => setTimeout(resolve, 800))
 					await rtgCommandHandler(ctx)
 					await await new Promise(resolve => setTimeout(resolve, 800))
-					rtsCommandHandler(ctx)
+					await rtsCommandHandler(ctx)
 					await new Promise(resolve => setTimeout(resolve, 800))
 					await ctx.replyWithAnimation(
 						'https://media1.tenor.com/m/KprNz_Lhdr4AAAAd/sukuna-gojo.gif'
