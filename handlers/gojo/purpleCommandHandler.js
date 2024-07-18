ef7d7fe6c761929d358b89889b2e40548fb007b8
soIoWAY <vitalikmur38@gmail.com>
2024-07-18 17:36:46 +0300

    upd purple

--- handlers/gojo/purpleCommandHandler.js
+++ handlers/gojo/purpleCommandHandler.js
@@ -1,6 +1,8 @@
 const sendMessages = require('../sendMessages')
 async function purpleCommandHandler(ctx) {
 	const username = ctx.message.from.username
+	const parts = ctx.message.text.split(' ')
+	const targetUsername = parts[1]
 	if (username !== 'xzvetal') {
 		await ctx.reply('Ти не Годжо Сатору!')
 	} else {
@@ -9,7 +11,9 @@ async function purpleCommandHandler(ctx) {
 			'Crow and declaration\nBetween front and back\nHollow... Purple',
 			'https://media1.tenor.com/m/DSyo0NKX8gMAAAAC/gojo-satoru.gif',
 		]
-		await sendMessages(ctx, messages, 800)
+		await sendMessages(ctx, messages, 850)
+		await new Promise(resolve => setTimeout(resolve, 1000))
+		await ctx.reply(`${username} влучив в ${targetUsername} фіолетовим`)
 	}
 }
 
