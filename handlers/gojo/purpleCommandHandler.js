ba664a58f9936f96f3e3d3852a0ec3142d697aa9
soIoWAY <vitalikmur38@gmail.com>
2024-07-18 22:31:01 +0300

    upd purple

--- handlers/gojo/purpleCommandHandler.js
+++ handlers/gojo/purpleCommandHandler.js
@@ -3,6 +3,7 @@ async function purpleCommandHandler(ctx) {
 	const username = ctx.message.from.username
 	const parts = ctx.message.text.split(' ')
 	const targetUsername = parts[1]
+	const usersWithRoles = ['@xzvetal', '@NightHanami', '@H4untt']
 	if (username !== 'xzvetal') {
 		await ctx.reply('Ти не Годжо Сатору!')
 	} else {
@@ -13,15 +14,19 @@ async function purpleCommandHandler(ctx) {
 		]
 		await sendMessages(ctx, messages, 850)
 		await new Promise(resolve => setTimeout(resolve, 1000))
-		if (targetUsername === '@H4untt') {
-			const isMahoraAdapt = Math.random()
-			if (isMahoraAdapt > 0.6) {
-				await ctx.reply('Махорага зміг адаптуватись')
+		if (usersWithRoles.includes(targetUsername)) {
+			if (targetUsername === '@H4untt') {
+				const isMahoraAdapt = Math.random()
+				if (isMahoraAdapt > 0.6) {
+					await ctx.reply('Махорага зміг адаптуватись')
+				} else {
+					await ctx.reply(`@${username} влучив в ${targetUsername} фіолетовим`)
+				}
 			} else {
 				await ctx.reply(`@${username} влучив в ${targetUsername} фіолетовим`)
 			}
 		} else {
-			await ctx.reply(`@${username} влучив в ${targetUsername} фіолетовим`)
+			await ctx.reply(`Фіолетовий розніс на атоми такого слабака`)
 		}
 	}
 }
