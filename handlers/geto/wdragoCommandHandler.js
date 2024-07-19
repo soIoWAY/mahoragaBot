9ae5b8202db5e9616927bdcdc2bfdc935f33f153
soIoWAY <vitalikmur38@gmail.com>
2024-07-19 12:40:45 +0300

    wdargo upd

--- handlers/geto/wdragoCommandHandler.js
+++ handlers/geto/wdragoCommandHandler.js
@@ -1,18 +1,35 @@
 async function wdragoCommandHandler(ctx) {
 	const username = ctx.message.from.username
+	const parts = ctx.message.text.split(' ')
+	const targetUsername = parts[1]
+	const usersWithRoles = ['@xzvetal', '@NightHanami', '@H4untt']
 	if (username !== 'NightHanami' && username !== 'xzvetal') {
 		await ctx.reply('Ти не Гето Сугуру!')
 	} else {
-		try {
-			await ctx.reply('Маніпуляція прокляттями... 👾')
-			await new Promise(resolve => setTimeout(resolve, 800))
-			await ctx.reply('Райдужний дракон!')
-			await new Promise(resolve => setTimeout(resolve, 800))
-			await ctx.replyWithAnimation(
-				'https://media1.tenor.com/m/Xjz7N5T75aIAAAAd/jujutsu-kaisen-season-2.gif'
-			)
-		} catch (error) {
-			console.error(error)
+		await ctx.reply('Маніпуляція прокляттями... 👾')
+		await new Promise(resolve => setTimeout(resolve, 800))
+		await ctx.reply('Райдужний дракон!')
+		await new Promise(resolve => setTimeout(resolve, 800))
+		await ctx.replyWithAnimation(
+			'https://media1.tenor.com/m/Xjz7N5T75aIAAAAd/jujutsu-kaisen-season-2.gif'
+		)
+		if (usersWithRoles.includes(targetUsername)) {
+			if (targetUsername === '@xzvetal') {
+				await ctx.replyWithAnimation(
+					'https://media1.tenor.com/m/tYFlGuVQpVcAAAAd/vfan-vfant.gif'
+				)
+			} else if (targetUsername == '@H4untt') {
+				const isAdapt = Math.random()
+				if (isAdapt > 0.5) {
+					await ctx.reply(`${targetUsername} знищив дракона розрізом`)
+				} else {
+					await ctx.reply(`Дракон зніс половину хп ${targetUsername}`)
+				}
+			} else {
+				await ctx.reply(`Райдужний дракон вбив ${targetUsername}`)
+			}
+		} else {
+			await ctx.reply('Така слабка мавпа стала кормом для моїх проклять')
 		}
 	}
 }
