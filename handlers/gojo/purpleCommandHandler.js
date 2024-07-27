d0415e2aee1822ae4c1cf4f024ecb67c26982c0f
soloWAY <vitalikmur38@gmail.com>
2024-07-28 00:15:52 +0300

    upd purple

--- handlers/gojo/purpleCommandHandler.js
+++ handlers/gojo/purpleCommandHandler.js
@@ -7,7 +7,9 @@ async function purpleCommandHandler(ctx) {
 	const supportUsername = parts[2]
 	const usernameRole = await getUserRole(username)
 	const sanitizedTargetUsername = targetUsername.replace(/^@/, '')
+	const sanitizedSupportUsername = targetUsername.replace(/^@/, '')
 	const targetUsernameRole = await getUserRole(sanitizedTargetUsername)
+	const supportUsernameRole = await getUserRole(sanitizedSupportUsername)
 	async function delay(ms) {
 		return new Promise(resolve => setTimeout(resolve, ms))
 	}
@@ -15,8 +17,8 @@ async function purpleCommandHandler(ctx) {
 		await ctx.reply('Ти не Годжо Сатору!')
 	} else {
 		const messages = [
-			'Phase twilight\nEyes of wisdom\nNine ropes',
-			'Crow and declaration\nBetween front and back\nHollow... Purple',
+			'Девять мотузок\nПоляризоване світло\nСпівають ворона і Шомо',
+			'Розрив між внутрішнім і зовнішнім\nТехніка пустоти... Фіолетовий 🟣',
 		]
 		await sendMessages(ctx, messages, 850)
 		await new Promise(resolve => setTimeout(resolve, 1000))
@@ -24,25 +26,27 @@ async function purpleCommandHandler(ctx) {
 			if (targetUsernameRole === 'sukuna') {
 				const isMahoraAdapt = Math.random()
 				let purple =
-					isMahoraAdapt <= 0.4
-						? 'https://media1.tenor.com/m/DSyo0NKX8gMAAAAC/gojo-satoru.gif'
-						: isMahoraAdapt > 0.4 && isMahoraAdapt <= 0.9
-						? 'https://media1.tenor.com/m/DSyo0NKX8gMAAAAC/gojo-satoru.gif'
-						: 'https://media1.tenor.com/m/Uk3MlwmQC90AAAAd/jujutsu-kaisen-ninjaristic.gif'
-				if (isMahoraAdapt <= 0.4) {
+					isMahoraAdapt <= 0.3
+						? 'https://media1.tenor.com/m/J5FKJH_pdZQAAAAd/gojo-satoru-gojo.gif'
+						: isMahoraAdapt > 0.3 && isMahoraAdapt <= 0.83
+						? 'https://media1.tenor.com/m/J5FKJH_pdZQAAAAd/gojo-satoru-gojo.gif'
+						: 'https://media1.tenor.com/m/Gr3phjzcytAAAAAC/manga-jujutsu-kaisen.gif'
+				if (isMahoraAdapt <= 0.3) {
 					await ctx.replyWithAnimation(purple)
 					await delay(850)
 					await ctx.reply('Махорага зміг адаптуватись')
-				} else if (isMahoraAdapt > 0.4 && isMahoraAdapt <= 0.9) {
+				} else if (isMahoraAdapt > 0.3 && isMahoraAdapt <= 0.83) {
 					await ctx.replyWithAnimation(purple)
 					await ctx.reply(`@${username} влучив в ${targetUsername} фіолетовим`)
-				} else if (isMahoraAdapt > 0.9 && supportUsername === '@Anasteyssha7') {
+				} else if (isMahoraAdapt > 0.9 && supportUsernameRole === 'utahime') {
 					await ctx.reply('УТАХІМЕ')
-					await delay(1000)
+					await delay(850)
+					await ctx.replyWithAnimation('https://i.imgur.com/lD6Q0q0.gif')
+					await delay(850)
 					await ctx.replyWithAnimation(purple)
 					await delay(850)
 					await ctx.reply(
-						`@${username} влучив в ${targetUsername} 200% фіолетовим, Махорага і ${targetUsername} були знищені`
+						`@${username} влучив в ${targetUsername} 200% фіолетовим, Махорага і ${targetUsername} були знищені.`
 					)
 				}
 			} else if (targetUsernameRole === 'geto') {
@@ -51,14 +55,14 @@ async function purpleCommandHandler(ctx) {
 				)
 			} else {
 				await ctx.replyWithAnimation(
-					'https://media1.tenor.com/m/DSyo0NKX8gMAAAAC/gojo-satoru.gif'
+					'https://media1.tenor.com/m/J5FKJH_pdZQAAAAd/gojo-satoru-gojo.gif'
 				)
 				await delay(850)
 				await ctx.reply(`@${username} влучив в ${targetUsername} фіолетовим`)
 			}
 		} else {
 			await ctx.replyWithAnimation(
-				'https://media1.tenor.com/m/DSyo0NKX8gMAAAAC/gojo-satoru.gif'
+				'https://media1.tenor.com/m/J5FKJH_pdZQAAAAd/gojo-satoru-gojo.gif'
 			)
 			await delay(850)
 			await ctx.reply(`Фіолетовий розніс на атоми такого слабака`)
