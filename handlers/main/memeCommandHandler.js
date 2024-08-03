929c7b6a85d5c1a50542a6e8bf0748887c57abea
soloway <vitalikmur38@gmail.com>
2024-08-03 23:08:03 +0300

    fix

--- handlers/main/memeCommandHandler.js
+++ handlers/main/memeCommandHandler.js
@@ -5,7 +5,7 @@ async function memeCommandHandler(ctx) {
 		const memeApi =
 			'https://www.reddit.com/r/dankmemes/top/.json?limit=1&t=hour'
 		const res = await axios.get(memeApi)
-		await ctx.replyWithPhoto(res.data.children[0].data.url_overridden_by_dest)
+		await ctx.replyWithPhoto(res.data.children[0].data.url)
 	} catch (err) {
 		console.error(err)
 	}
